a8b768e09976309277ea2cdaf21e03d8f43e0d4c
mosradish <mosradish@gmail.com>
2025-01-21 15:44:28 +0900

    slick change

---
+++ js/slickChange.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    const breakpoint = 1024; // モバイルサイズの基準
+    let slickInitialized = false;
+
+    function initializeSlick() {
+        if ($(window).width() <= breakpoint && !slickInitialized) {
+            $('.slick_change').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+            });
+            slickInitialized = true;
+        } else if ($(window).width() > breakpoint && slickInitialized) {
+            $('.slick_change').slick('unslick'); // スライダー解除
+            slickInitialized = false;
+        }
+    }
+
+    // 初期化
+    initializeSlick();
+
+    // リサイズ時にチェック
+    $(window).resize(initializeSlick);
+});
