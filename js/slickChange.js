63e009db3e9d68620b16da60120891707a2ac678
mosradish <mosradish@gmail.com>
2025-01-22 14:13:35 +0900

    add slick

--- js/slickChange.js
+++ js/slickChange.js
@@ -1,37 +1,34 @@
 $(document).ready(function () {
     const breakpoint = 1024; // モバイルサイズの基準
-    let slickHidden = false;
 
     function toggleDisplay() {
-        if ($(window).width() <= breakpoint && !slickHidden) {
-            $('.slick_change').css('display', 'none');
-            slickHidden = true;
+        if ($(window).width() <= breakpoint) {
+
             $('.slick_slides_works').slick({
                 slidesToShow: 1,
                 centerMode: true,
                 centerPadding: "25%",
                 slidesToScroll: 1,
-                arrows: true,
+                arrows: false,
                 dots: true,
                 autoplay: false,
                 infinite: true,
                 speed: 500,
+                draggable: true,
             });
 
             $('.slick_slides_florist').slick({
                 slidesToShow: 1,
                 centerMode: true,
-                centerPadding: "30%",
+                centerPadding: "25%",
                 slidesToScroll: 1,
-                arrows: true,
+                arrows: false,
                 dots: true,
                 autoplay: false,
                 infinite: true,
                 speed: 500,
+                draggable: true,
             });
-        } else if ($(window).width() > breakpoint && slickHidden) {
-            $('.slick_change').css('display', '');
-            slickHidden = false;
         }
     }
 
