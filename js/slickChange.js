1b3b7cf0659b1ba57e660fa7a5671cba1f701cfc
mosradish <mosradish@gmail.com>
2025-01-24 15:14:06 +0900

    js center padding change

--- js/slickChange.js
+++ js/slickChange.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
             function initSlider() {
                 let imageWidth = imgElement.width(); // 画像の幅を取得
-                let centerPadding = (imageWidth / 2) + 20 + 'px'; // 画像の半分 + 20px
+                const centerPadding = (imageWidth / 2) + (2 * (viewportWidth / 100)) + 'px'; // 画像の半分 + 2vw
 
                 if ($(sliderClass).length && !$(sliderClass).hasClass('slick-initialized')) {
                     $(sliderClass).slick({
