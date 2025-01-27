22e1995f97455fae21e4859b2c38e9aeac445490
mosradish <mosradish@gmail.com>
2025-01-27 12:39:55 +0900

    slick change

--- js/slickChange.js
+++ js/slickChange.js
@@ -2,22 +2,25 @@ $(document).ready(function () {
     const breakpoint = 1024; // モバイルサイズの基準
 
     function initializeSlick(sliderClass, imageSelector) {
+        // 画像の読み込みが完了した後に処理を行う
         $(imageSelector).each(function () {
             const imgElement = $(this);
 
+            // ビューポート幅を取得
             const viewportWidth = $(window).width();
             const vwToPx = viewportWidth * 0.02; // 2vw をピクセル単位に変換
             
+            // 画像の読み込みを確認
             if (!imgElement[0].complete) {
                 imgElement.on('load', function () {
                     initSlider();
                 });
             } else {
-                initSlider();
+                initSlider(); // すでにロードされている場合
             }
 
             function initSlider() {
-                let imageWidth = imgElement.width();
+                let imageWidth = imgElement.width(); // 画像の幅を取得
                 const centerPadding = (imageWidth / 2) + (viewportWidth / 100) + 'px';
 
                 if ($(sliderClass).length && !$(sliderClass).hasClass('slick-initialized')) {
@@ -48,9 +51,8 @@ $(document).ready(function () {
 
         if (viewportWidth <= breakpoint) {
             // Works スライダー
-
             $('.slide_items_work').width(worksWidth);
-            $('.slide_items_work').css('max-width', workstWidth + 'px');
+            $('.slide_items_work').css('max-width', worksWidth + 'px');
             initializeSlick('.slick_slides_work', '.slide_items_work img');
 
             // Florist スライダー
